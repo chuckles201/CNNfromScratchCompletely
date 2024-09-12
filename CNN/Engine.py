aaee0da97358d4b2b9de3aa39248d91a94dd2c9c
chuckles201 <42980649+chuckles201@users.noreply.github.com>
2024-09-11 20:06:15 -0400

    Update and rename Engine to Engine.py

---
+++ CNN/Engine.py
@@ -0,0 +1,24 @@
+'''This file will have the custom-class that we will backpropogate through eventually, and use to compute the transformations caused by layers. Similar to a 
+a PyTorch Tensor, but much simpler!
+
+The operations we need:
+
+'''
+
+
+# Parent class to store our data and preform transformations on it. (doesn't support broadcasting)
+
+class AwesomeHolder: 
+  def __init__(values):
+    self.values = values
+    
+  def matrixmul():
+
+  def add():
+
+  def sub():
+
+  def divide():
+
+  def multiply():
+
